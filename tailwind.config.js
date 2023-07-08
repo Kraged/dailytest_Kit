6fc325d69186445c9cdf17338ef78ee2742e0fac
Kraged <35677858+Kraged@users.noreply.github.com>
2023-07-08 21:54:36 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  purge: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,7 +8,6 @@ module.exports = {
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
