0bffbd7191753cd6feb30fca9b11a0e786ed4860
Kraged <35677858+Kraged@users.noreply.github.com>
2023-07-08 21:50:17 +0800

    Fixing Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/*.{js,ts,jsx,tsx,mdx}",
+  ],
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
